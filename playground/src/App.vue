32558155817cdd3d6c8c8a51a55664ab3edb3ee8
Alex Liu <dsa1314@gmail.com>
2023-04-30 22:22:52 +0800

    chore: initial playground

---
+++ playground/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// Edit this file to play around with vite-async-component-loader but never commit changes!
+// import LazyComponent from './components/Component.vue?async'
+</script>
+
+<template>
+  <div>
+    <!-- <LazyComponent /> -->
+  </div>
+</template>
